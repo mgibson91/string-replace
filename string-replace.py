a0b8fb213e610550aa623e717c4112b6f245cd5f
mgibson91 <matthewgibson91@gmail.com>
2017-09-26 23:19:25 +0100

    Removed delimiters, now continuously search for user defined strings. Also refactored logic into a class to make for easier extension

--- string-replace.py
+++ string-replace.py
@@ -1,147 +1,78 @@
 import sys
-sys.path.append('pyperclip-1.5.27')
-
-import pyperclip
-
-import pyxhook
-
 import subprocess
 import json
 
+import pyxhook
+sys.path.append('pyperclip-1.5.27')
+import pyperclip
 
-logFileName    ='/tmp/keys-history.log'
-delimiter      = '-'
-currentString  = ''
-aliases        = {}
+from base import AliasKeyListener
 
-exitCount      = 0
-
-def getReplacements(configFile):
-
-    with open(configFile) as data_file:    
-        data = json.load(data_file)
-
-    if 'aliases' not in data:
-        return 'No config file found.', {}
-
-    aliasReplacements = data['aliases']
-
-    values = {}
-
-    for alias in aliasReplacements:
-        values[alias] = aliasReplacements[alias]
-
-    return '', values
+debug       = False
+logFileName = '/tmp/keys-history.log'
 
 
-# Returns:
-# - Boolean determining whether a replacement match was found
-# - Replacement string
-def checkForReplacement(alias):
-
-  if (alias in aliases):
-    return True, aliases[alias]
-
-  return False, ''
+exitCount = 0
 
 # Using 'BackSpace' remove the required number of characters
 def removeCharacters(numChars):
 
-  for i in range(numChars):
-    subprocess.check_output('/usr/bin/xdotool key BackSpace', shell=True)
-    
+    for _ in range(numChars):
+        subprocess.check_output('/usr/bin/xdotool key BackSpace', shell=True)
+
 # Paste replacement string
-def pasteReplacement(replacement):
+def pasteReplacement(replacement, isTerminal):
 
-  windowName = subprocess.check_output('xdotool getwindowname $(xdotool getwindowfocus)', shell=True)
+    if isTerminal:
+        subprocess.check_output(
+            "echo \"" + replacement + "\" > /tmp/temp-string-replace",  shell=True)
+        subprocess.check_output(
+            "xsel --input < /tmp/temp-string-replace",                  shell=True)
 
-  # Window name containing '@' is most likely a terminal. SSH session terminals don't return a window name
-  if '@' in windowName or len(windowName) == 0:
-    subprocess.check_output("echo \"" + replacement + "\" > /tmp/temp-string-replace",  shell=True)
-    subprocess.check_output("xsel --input < /tmp/temp-string-replace",                  shell=True)
+    # Use pyperclip for GUI applications
+    else:
+        pyperclip.copy(replacement)
 
-  # Use pyperclip for GUI applications
-  else:
-    pyperclip.copy(replacement)
-
-  subprocess.check_output('/usr/bin/xdotool key --clearmodifiers Shift+Insert', shell=True)
-
-def replaceString(alias, replacement):
-
-  # Remove alias. Bear in mind we need to add 2 characters to the remove length to account for the two already stripped '-' characters
-  removeCharacters(len(alias) + 2)
-  pasteReplacement(replacement)
+    subprocess.check_output(
+        '/usr/bin/xdotool key --clearmodifiers Shift+Insert', shell=True)
 
 
-def log(message, file):
+def replaceString(removeLength, replacement, isTerminal):
+    removeCharacters(removeLength)
+    pasteReplacement(replacement, isTerminal)
 
-  if (file):
-    file.write(message)
+# def log(message, logFile):
+#     if (logFile):
+#         logFile.write(message)
+
 
 # This function is called everytime a key is pressed.
 def OnKeyPress(event):
 
-  global currentString
-  global exitCount
+    global currentString
+    global exitCount
 
-  logFile=open(logFileName,'a')
+    # logFile = open(logFileName, 'a')
 
-  if event.Ascii == 0:
-    if len(currentString) > 0:
-      currentString = currentString[:-1]
+    isMatch, replacement = aliasKeyListener.logKey(event)
 
-    log('Current alias: {}\n'.format(currentString), logFile)
-    return
+    if (isMatch):
+        isTerminal = 'term' in event.WindowProcName
+        replaceString(replacement.removeLength, replacement.text, isTerminal)
 
-  currentChar = chr(event.Ascii)
-  isDelimiter = (currentChar == delimiter)
+    if chr(event.Ascii) == '`':
+        exitCount += 1
 
-  # If we already have an alias or this character is a delimiter, add to currentString
-  if (delimiter in currentString) or isDelimiter:
+        if exitCount > 1:
+            # logFile.close()
+            new_hook.cancel()
+    else:
+        exitCount = 0
 
-    currentString += currentChar
+aliasKeyListener = AliasKeyListener('config.json')
 
-    # If string is at least 3 characters, check to see if a 
-    if len(currentString) > 1 and isDelimiter:
-
-      log('Two delimiters found, checking for match\n', logFile)
-
-      # Remove last and first delimiter chars
-      alias = currentString[:-1][1:]
-      found, replacement = checkForReplacement(alias)
-
-      if (found):
-        log('Replacement found\n', logFile)
-        replaceString(alias, replacement)
-      else:
-        log('No replacement found\n', logFile)
-      
-      # Reset current alias
-      currentString = ''
-
-  if len(currentString) > 0:
-    log('Current alias: {}\n'.format(currentString), logFile)
-
-  if currentChar=='`':
-    exitCount += 1
-
-    if exitCount > 1:
-      logFile.close()
-      new_hook.cancel()
-  else:
-    exitCount = 0
-
-
-##### ENTRY POINT #####
-
-# Get aliases and replacements from config file
-errorMsg, aliases = getReplacements('config.json')
-
-if errorMsg:
-  print ('Error reading config file. Exiting. {}'.format(errorMsg))
-
-
-new_hook=pyxhook.HookManager()  # Instantiate HookManager class
-new_hook.KeyDown=OnKeyPress     # Listen to all keystrokes
-new_hook.HookKeyboard()         # Hook the keyboard
-new_hook.start()                # Start the session
+#Start
+new_hook = pyxhook.HookManager()  # Instantiate HookManager class
+new_hook.KeyDown = OnKeyPress     # Listen to all keystrokes
+new_hook.HookKeyboard()           # Hook the keyboard
+new_hook.start()                  # Start the session
